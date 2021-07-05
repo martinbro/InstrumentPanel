6e319aa80917e7d9d30fa484b51e409959b7e294
Martin <martinbromikkelsen.com>
2021-07-05 15:55:11 +0200

    IndstilBox/Indstl ermed forbedret layout

--- src/component/Indst.svelte
+++ src/component/Indst.svelte
@@ -1,67 +1,73 @@
 <script lang="ts">
-import { fly } from 'svelte/transition';
 import {
     isFixed,fixedLat,fixedLng,
     isVisGyro, isVisFluxgate,isVisRawFluxgate,
     radius,antalVektorer,
-    isVisGPS,antalGPSpos,aktuelAntalGPSpos} from "../stores/indstStore"
+    isVisGPS,antalGPSpos,aktuelAntalGPSpos,point,hidefixpos} from "../stores/indstStore"
 import type {IGPS} from "../Interfaces/interfaces"
 import IndstBox from "./IndstBox.svelte"
 
 export let gps:IGPS;
+let showRetPos=false;
 
 $: antalMeter = Math.round($radius*1852/60*10)/10;
 $: $isFixed, fixPos();
 
+$: $point, setPoint();
+function setPoint() {
+    // $fixedLng = $point.lng;
+    if($isFixed && $hidefixpos){
+        $fixedLat = $point.lat/1;
+        $fixedLng = $point.lng/1;
+        $hidefixpos = false;
+    }
+    console.log($point.lat,$point.lng,$fixedLat,$fixedLng);
+    
+}
+
 let fixPos = ()=>{
-    if($isFixed){
+    if($isFixed && $fixedLng==0){
         $fixedLat = gps.lat;
         $fixedLng = gps.lng;
+
     }
 }
 </script>
-
+<!-- <head><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></head> -->
 <!-- *************************************************************************************************************** -->
 
 <h2>Kort indstillinger</h2>
-<IndstBox title="Vis Vektorer" >
-    <label> 
-        <input class = "container" type = "checkbox" bind:checked = "{$isVisGyro}">
-        Vis Gyrokompas
-    </label>
-    <label> 
-        <input class = "container" type = "checkbox" bind:checked = "{$isVisFluxgate}">
-        Vis Fluxgate
-    </label>
-    <label> 
-        <input class = "container" type = "checkbox" bind:checked = "{$isVisRawFluxgate}">
-        Vis Fluxgate (ikke stabiliseret)
-    </label>
-</IndstBox>
+<IndstBox title="Vis Gyrokompas" bind:checked="{$isVisGyro}" ></IndstBox>
+<IndstBox title="Vis Fluxgate" bind:checked="{$isVisFluxgate}"  ></IndstBox>
+<IndstBox title="Vis Fluxgate (ikke stabiliseret)" bind:checked="{$isVisRawFluxgate}"  ></IndstBox>
 
-<IndstBox title="Fasthold position" bind:checked = "{$isFixed}">
-    <div class = "panel" transition:fly ="{{ y: -25, duration: 200 }}">
+<IndstBox title="Fasthold position" show_ret="{$isFixed?true:false}" bind:ret="{$hidefixpos}" bind:checked = "{$isFixed}">
+
+    <div slot ="ret">
+        <small>
+
+        <p>Indtast bredde og længde</p>
         <label>
-            <p>Indtast bredde og længde</p>
-            <span>Lat: </span> 
+            <span>Lat:</span> 
             <input type="number" step=0.00001 bind:value="{$fixedLat}">
         </label>
         <label>
             <span>Lng:</span>     
             <input type="number" step=0.00001 bind:value="{$fixedLng}">
         </label>
+                        
+        </small>
+        <hr>
     </div>
 </IndstBox>
 
 <IndstBox title="Antal viste vektorer">
-    <!-- <div class = "panel" transition:fly ="{{ y: -25, duration: 200 }}"> -->
-        <label>
-            <p>Antal vektorer der skal vises:</p> 
-            <input type=number bind:value={$antalVektorer} min=1 max=100>
-            <input type=range bind:value={$antalVektorer} min=1 max=100>
-        </label>
-        <!-- </div> -->
-</IndstBox>
+    <label>
+        <p>Antal vektorer der skal vises:</p> 
+        <input type=number bind:value={$antalVektorer} min=1 max=100>
+        <input type=range bind:value={$antalVektorer} min=1 max=100>
+    </label>
+ </IndstBox>
 
 <IndstBox title="Vektorlængde">
     <label>
@@ -71,37 +77,41 @@ let fixPos = ()=>{
     </label>
     <p>længden: {antalMeter}m</p>
 </IndstBox>
-<hr>
+
 <IndstBox title="Vis GPS position" bind:checked={$isVisGPS}>
     <label>
         <p>Max. antal viste positioner:</p> 
         <input type=number bind:value={$antalGPSpos} min=1 max=1000>
         <input type=range bind:value={$antalGPSpos} min=1 max=1000>
     </label>
-    <p> <small>Aktuel antal viste GPS pos.: {$aktuelAntalGPSpos}</small> </p>
+    <p><small>Aktuel antal viste GPS pos.: {$aktuelAntalGPSpos}</small> </p>
 </IndstBox>
  
 <!-- ***************************************************************************************** -->
 
 <style>
-    h2{
-        margin: 0;
-        width:11em
-    }
+    /* span{
+        display: block;
+        width: 40px;
+    } */
 
     span{
         display: inline-block;
         width:2em;  
     }
 
-    .panel{
+  
+    h2{
         margin: 0;
-        padding: 0 15px 15px 15px;
-        background-color: wheat;
-    } 
+        width:11em
+    }
+
+
+
     p{
         margin: 0;
         padding: 0;
     }
+
     
 </style>
