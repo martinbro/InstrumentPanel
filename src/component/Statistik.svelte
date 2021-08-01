4a01a8f6ca688d307ed91b9a6ca4f4bc84ef4de0
Martin <martinbromikkelsen.com>
2021-08-01 09:32:21 +0200

    Waypoints og input til gyro tilføjet

---
+++ src/component/Statistik.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {point,WayPoints,isVisWaypoints,crosshair,hidefixpos} from "../stores/tsStore"
+    import type {IPos} from "../Interfaces/interfaces"
+    import IndstBox from "./IndstBox.svelte"
+    
+</script>
+<h2>Statistik</h2>
+<IndstBox title="Gyrokompasdata">
+    
+</IndstBox>
+
+
+<style>
+    h2{
+        margin: 0;
+        width:11em
+    }
+</style>
---
+++ src/component/Styring.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    // import {point,WayPoints,isVisWaypoints,crosshair,hidefixpos} from "../stores/tsStore"
+    // import type {IPos} from "../Interfaces/interfaces"
+    import IndstBox from "./IndstBox.svelte"
+</script>
+<h2>Styring</h2>
+<IndstBox title="P-værdi"> 
+    <slot>
+    </slot>
+</IndstBox>
+<IndstBox title="I-værdi">
+    <slot>
+    </slot>
+</IndstBox>
+<IndstBox title="D-værdi">
+    <slot>
+    </slot>
+</IndstBox>
+
+<style>
+    h2{
+        margin: 0;
+        width:11em
+    }
+</style>
